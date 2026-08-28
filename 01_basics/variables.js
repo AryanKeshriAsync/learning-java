07915566b1267cf3bacde038009c74e9c4dd0263
Aryan Keshri <aryankeshri5716@gmail.com>
2026-08-28 12:48:51 +0000

    Learned Variables & const

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId=12586
+let accountEmail="xyz@google.com"
+var accountPass="12345"
+accountCity="New Delhi"
+let accountName
+
+// accountId=25852
+
+/*only use 'let' for variables, because of no scope problem*/
+
+accountEmail="aryan@yahoo.co.in"
+accountName="Aryan Keshri"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountName])
