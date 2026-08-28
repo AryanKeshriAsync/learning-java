e0439ed84ed35ef478a41d0b495695fff8cd1f9c
Aryan Keshri <aryankeshri5716@gmail.com>
2026-08-28 13:30:05 +0000

    learned datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)// we re using nodejs, not a browser
+
+console.log(3+3);
+console.log("aryan"); //code readibility should be high
+
+let name="Aryan Keshri" //string
+let number=55 //number
+let islearning=true //boolean
+let temp=null
+
+//number=> 2 to power of 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+console.log(temp);
+
+
