dc905d5883c9ace267fe9b1d060eeff788a85246
Aryan Keshri <aryankeshri5716@gmail.com>
2026-08-29 11:05:54 +0000

    datatypes conversion

--- 01_basics/conversionOp.js
+++ 01_basics/conversionOp.js
@@ -1,7 +1,34 @@
-let score="33"
+// let score="aryan"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber= Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1, false=>0
+
+// let isLoggedIn= "aryan"
+
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+// console.log(typeof isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//1=>true, 0=>false
+//""=>false
+//"aryan"=>true
+
+let aNumber=33
+let string=String(aNumber)
+
+console.log(typeof string)
+console.log(string);
+
+
+
+
