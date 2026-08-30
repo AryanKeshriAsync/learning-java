b18097c4977ea5a9d4b6d5bffb8518c542ac56f3
Aryan Keshri <aryankeshri5716@gmail.com>
2026-08-30 18:18:31 +0000

    learned comparisions

---
+++ 01_basics/comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1); //true
+// console.log(2>3); //false
+// console.log(2>=2); //true
+// console.log(2>2); //false
+// console.log(2==2); //true
+// console.log(2!=3); //true
+// console.log(2==4); //false
+// console.log(2!=2); //false
+
+console.log("2"===2); //=== strict check, check datatype too
+console.log("2"==="2");
+
+//avoid comparison btw diff datatypes
+
+
+
+
+
+
+
+
+
