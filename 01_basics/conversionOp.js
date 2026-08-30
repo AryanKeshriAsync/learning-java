2ceae11b03016a24cbd4d0a8a15162537921ed59
Aryan Keshri <aryankeshri5716@gmail.com>
2026-08-30 17:43:03 +0000

    learned conversions in js

--- 01_basics/conversionOp.js
+++ 01_basics/conversionOp.js
@@ -26,8 +26,46 @@
 let aNumber=33
 let string=String(aNumber)
 
-console.log(typeof string)
-console.log(string);
+// console.log(typeof string)
+// console.log(string);
+
+
+//********************Operations********************************
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //2 to the power 3
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello"
+let str2=" aryan"
+let str3= str1+str2
+// console.log(str3);
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+"2"); //12
+// console.log(1+"2"+"3"); //123
+// console.log("1"+2+3); //123
+// console.log(1+2+"3"); //33
+// console.log(1+2+"3"+"4"); //334
+
+// console.log(+true);
+// console.log(+"");
+
+ //dont write confusing and messy codes 
+
+let a=10;
+console.log(a++);
+console.log(a++);
+
+
 
 
 
