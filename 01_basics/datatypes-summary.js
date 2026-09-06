30ce2bd3d0beed673b70ead5fe2d5ebc032b3b4a
Aryan Keshri <aryankeshri5716@gmail.com>
2026-09-06 18:46:18 +0000

    summary of datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+//Primitive
+
+//7 types- string,number,boolean,null,undefined,symbol,BigInt
+
+const score=100 //number
+const scoreValue=200.4 //number
+
+const isLoggedIn=false //bool
+const outsideTemp=null //null
+let username //undefined
+const userEmail=undefined
+
+const Id=Symbol('123')
+const anotherId=Symbol('123')
+
+const bigNo=325641258976528776n
+
+// console.log(Id===anotherId); //false
+
+
+
+//non primitive
+
+//array,objects,functions
+
+let arr=["aryan","virat","emraan","rdj"]
+let myObj = {
+    name:"Aryan",
+    rollNo:21,
+    isStudent:true
+}
+
+console.log(arr);
+console.log(myObj);
+console.log(typeof bigNo);
+
+
+
