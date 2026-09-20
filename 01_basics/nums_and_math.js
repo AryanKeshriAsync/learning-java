c80456ad9543fccabc05146e89ff06686eb62cca
Aryan Keshri <aryankeshri5716@gmail.com>
2026-09-20 18:42:57 +0000

    learned nums and maths

--- 01_basics/nums_and_math.js
+++ 01_basics/nums_and_math.js
@@ -17,9 +17,31 @@ const hundreds=1000000000
 
 //++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-10));
-console.log(Math.round(25.78));
+// console.log(Math);
+// console.log(Math.abs(-10));
+// console.log(Math.round(3.4));
+// console.log(Math.ceil(3.2));
+// console.log(Math.floor(3.8));
+
+// console.log(Math.min(2,3,-4,1,7));
+// console.log(Math.max(2,3,-4,1,7));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+const min=10
+const max=20
+
+console.log(Math.floor((Math.random()*(max-min+1))+min));
+
+
+
+
+
+
+
 
 
 
