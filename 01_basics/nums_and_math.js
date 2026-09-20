d03f64429d53ce4708281b9c9f12e32cf3a837e8
Aryan Keshri <aryankeshri5716@gmail.com>
2026-09-20 13:12:49 +0000

    learned maths

---
+++ 01_basics/nums_and_math.js
@@ -0,0 +1,25 @@
+const score=500
+const score2=new Number(452.89568)
+// console.log(score);
+// console.log(score2);
+
+// console.log(score2.toString());
+// console.log(score2.toString().length);
+// console.log(score2.toString()[3]);
+// console.log(score2.toFixed(2));
+
+const otherNo=123.8651
+// console.log(otherNo.toPrecision(3));
+
+const hundreds=1000000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-10));
+console.log(Math.round(25.78));
+
+
+
