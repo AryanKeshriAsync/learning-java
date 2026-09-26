942181166bfa81693cca2f752c08e0b9d79c0efa
Aryan Keshri <aryankeshri5716@gmail.com>
2026-09-26 18:30:51 +0000

    arrays continued

---
+++ 02_basics/arrays2.js
@@ -0,0 +1,21 @@
+let arr=["ironman","batman","war machine"]
+let arr2=["black widow","wonder woman", "poison ivy"]
+
+//  arr.push(arr2)
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[3][0]);
+
+// let arr3= arr.concat(arr2)
+// console.log(arr3);
+// console.log(arr3.length);
+// console.log(arr3[4]);
+
+// let arr3=[...arr, ...arr2]  //spread operator
+// console.log(arr3);
+
+const newArr=[1,2,3,[4,5,6],[7,[8,9]],10]
+const newArr2=newArr.flat(Infinity)    //flat operator, depth
+console.log(newArr2); 
+
+
